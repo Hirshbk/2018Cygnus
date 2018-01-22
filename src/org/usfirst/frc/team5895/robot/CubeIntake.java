0cfbd41f8be69f367d06e1ca86d887a533fb662a
bmacedo-18 <35277180+bmacedo-18@users.noreply.github.com>
2018-01-22 16:53:25 -0500

    Intake
    Added Intake and solenoid to pinch on cube

---
+++ src/org/usfirst/frc/team5895/robot/CubeIntake.java
@@ -0,0 +1,63 @@
+package org.usfirst.frc.team5895.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+
+public class CubeIntake {
+	private static enum Mode_Type {INTAKING, EJECTING, HOLDING};
+	private Mode_Type mode = Mode_Type.EJECTING;
+	private Talon motor;
+	private Solenoid solenoid;
+	private boolean solenoidState;
+	private DigitalInput sensor;
+	
+	public CubeIntake (){
+		motor = new Talon(3);
+		solenoid = new Solenoid (0);
+		sensor = new DigitalInput(4); 	//* eventually will use analog sensor 
+		}
+
+	public void intake(){
+		mode = Mode_Type.INTAKING;
+		}
+	
+	public void eject(){
+		mode= Mode_Type.EJECTING;
+		}
+	
+	public void update(){
+		if(mode != Mode_Type.EJECTING) {
+			if (! sensor.get()) {
+				mode = Mode_Type.HOLDING;
+			}else {
+				mode = Mode_Type.INTAKING;	
+			}
+		}
+		
+		double motorspeed = 0; 
+		switch(mode) {
+		
+		case INTAKING:
+			motorspeed = 1;
+			solenoidState = false;
+			break;
+		
+		case HOLDING:
+			motorspeed = .25;
+			solenoidState = true; //* solenoid used to clamp cube while holding 
+			break;
+		
+		case EJECTING:
+			motorspeed = -1;
+			solenoidState = false; 
+			break; 
+			}
+	
+		motor.set(motorspeed);
+		solenoid.set(solenoidState);
+		
+	}
+}
+	
+
--- src/org/usfirst/frc/team5895/robot/Robot.java
+++ src/org/usfirst/frc/team5895/robot/Robot.java
@@ -12,15 +12,19 @@ public class Robot extends IterativeRobot {
 	
 	Looper loop;
 	Elevator elevator;
-	Joystick joystick;
+	Joystick ljoystick;
+	Joystick rjoystick;
+	CubeIntake intake;
 	
 	public void robotInit() {
-		
+		intake = new CubeIntake();
 		elevator = new Elevator();
-		joystick = new Joystick(0);
+		ljoystick = new Joystick(0);
+		rjoystick = new Joystick(1);
 		
 		loop = new Looper(10);
 		loop.add(elevator::update);
+		loop.add(intake::update);
 		loop.start();
 	}
 
@@ -34,16 +38,16 @@ public class Robot extends IterativeRobot {
 		//elevator motion magic
 		double leftYstick = -1.0 * joystick.getRawAxis(1);
 		
-		if(joystick.getRawButton(1)) {
+		if(rjoystick.getRawButton(1)) {
 			elevator.setTargetPosition(0);
 			}
-			else if(joystick.getRawButton(2)) {
+			else if(rjoystick.getRawButton(2)) {
 				elevator.setTargetPosition(30);
 			}
-			else if(joystick.getRawButton(3)) {
+			else if(rjoystick.getRawButton(3)) {
 				elevator.setTargetPosition(45);
 			}
-			else if(joystick.getRawButton(4)) {
+			else if(rjoystick.getRawButton(4)) {
 				elevator.setTargetPosition(60);
 			}
 			else {
@@ -51,6 +55,12 @@ public class Robot extends IterativeRobot {
 				elevator.driverControl(leftYstick);
 			}
 			
+		if(ljoystick.getRawButton(1)) {
+			intake.eject();
+			}
+			else if(ljoystick.getRawButton(2)) {
+			intake.intake();
+			}
 
 	}
 
