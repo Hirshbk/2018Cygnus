8e96d43f2ce1541cf23f263851335ff6ab5608e7
bmacedo-18 <35277180+bmacedo-18@users.noreply.github.com>
2018-01-24 17:01:47 -0500

    added timer to intake
    The Talented Timer Tells The Intelligent Intake To willingLY Wait (200ms) Before Hotly Holding

--- src/org/usfirst/frc/team5895/robot/CubeIntake.java
+++ src/org/usfirst/frc/team5895/robot/CubeIntake.java
@@ -5,15 +5,18 @@ import org.usfirst.frc.team5895.robot.lib.DistanceSensor;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
 
 public class CubeIntake {
-	private static enum Mode_Type {INTAKING, EJECTING, HOLDING};
+	private static enum Mode_Type {INTAKING, EJECTING, HOLDING, WAITING};
 	private Mode_Type mode = Mode_Type.EJECTING;
 	private Talon motor1, motor2;
 	private Solenoid solenoid;
 	private boolean solenoidState;
 	private DistanceSensor sensor1, sensor2;
-	double motorspeed1, motorspeed2, volts1, volts2, x1, x2, avgDist, speed;
+	private Timer timer;
+	double motorspeed1, motorspeed2, volts1, volts2, x1, x2, avgDist, speed, lastTime;
+	boolean haveCube;
 	
 	public CubeIntake (){
 		motor1 = new Talon(3);
@@ -31,14 +34,12 @@ public class CubeIntake {
 		mode= Mode_Type.EJECTING;
 		}
 	
+	public void waiting(){
+		double lastTime = timer.getFPGATimestamp();
+		mode = Mode_Type.WAITING; 
+	}
+	
 	public void update(){
-		if(mode != Mode_Type.EJECTING) {
-			if (sensor1.getDistance() < 5 ||sensor2.getDistance() < 5) {
-				mode = Mode_Type.HOLDING; // test to see if this should be "and" statement or "or"
-			}else {
-				mode = Mode_Type.INTAKING;	
-			}
-		}
 		
 		motorspeed1 = 0;
 		motorspeed2 = 0;
@@ -49,20 +50,51 @@ public class CubeIntake {
 			 x1 = sensor1.getDistance();
 		     x2 = sensor2.getDistance();
 		     
-			 if (x1 < 20 || x2 < 20) {
+		     if(x1 < 5 || x2 < 5) {
+		    	 haveCube = true;
+		     }
+		     else if ( (x1 < 20 && x1>= 5)|| (x2 < 20 && x2>=5)) {
 		            
 		            avgDist = (x1+x2)/2;
 		            speed = 16.316*Math.pow(avgDist+20, -0.844367);
 		            
 		            motorspeed1 =  speed/4 + x1/45;
-		            motorspeed2 = speed/4 + x2/45;
-		            
-		        } else {
+		            motorspeed2 = speed/4 + x2/45;    
+		      }
+		     else {
 		            motorspeed1 = 1.0;
 		            motorspeed2 = 1.0;        
-		        }
-		        
+		      }
+			
+			 if (haveCube){
+				lastTime = timer.getFPGATimestamp();
+			 	mode = Mode_Type.WAITING;
+			 }
+			
 			solenoidState = false;
+			break;
+			
+		case WAITING:
+		
+			x1 = sensor1.getDistance();
+		    x2 = sensor2.getDistance();
+		    
+			avgDist = (x1+x2)/2;
+            speed = 16.316*Math.pow(avgDist+20, -0.844367);
+            
+            motorspeed1 =  speed/4 + x1/45;
+            motorspeed2 = speed/4 + x2/45;
+            
+        	double curTime = timer.getFPGATimestamp();
+            if (curTime - lastTime > 0.2) {
+            	if (haveCube) {
+            		mode = Mode_Type.HOLDING;
+            	}else {
+            		mode = Mode_Type.WAITING;
+            	}
+            }
+            
+            solenoidState=false;
 			break;
 		
 		case HOLDING:
