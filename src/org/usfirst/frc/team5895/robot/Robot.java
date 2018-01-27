edfe42c69183823c7f59e9764b3f63384a2af58b
bmacedo-18 <35277180+bmacedo-18@users.noreply.github.com>
2018-01-27 15:36:21 -0500

    Added Driver Control/Buttons
    Best Buttons by Briana

--- src/org/usfirst/frc/team5895/robot/Robot.java
+++ src/org/usfirst/frc/team5895/robot/Robot.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team5895.robot;
 
 import org.usfirst.frc.team5895.robot.framework.Looper;
+import org.usfirst.frc.team5895.robot.lib.BetterJoystick;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -10,15 +12,21 @@ public class Robot extends IterativeRobot {
 	
 	Looper loop;
 	Elevator elevator;
-	Joystick ljoystick;
-	Joystick rjoystick;
+	BetterJoystick ljoystick;
+	BetterJoystick rjoystick;
 	CubeIntake intake;
+	HatHatHat hat;
+	Claw claw;
+	boolean hatMode;
+	boolean clawUp;
 	
 	public void robotInit() {
 		intake = new CubeIntake();
 		elevator = new Elevator();
-		ljoystick = new Joystick(0);
-		rjoystick = new Joystick(1);
+		ljoystick = new BetterJoystick(0);
+		rjoystick = new BetterJoystick(1);
+		hat = new HatHatHat();
+		claw = new Claw();
 		
 		loop = new Looper(10);
 		loop.add(elevator::update);
@@ -36,29 +44,50 @@ public class Robot extends IterativeRobot {
 		//elevator motion magic
 		double leftYstick = -1.0 * rjoystick.getRawAxis(1);
 		
-		if(rjoystick.getRawButton(1)) {
-			elevator.setTargetPosition(0);
+		if(rjoystick.getRisingEdge(2)) {
+				elevator.setTargetPosition(66);
 			}
-			else if(rjoystick.getRawButton(2)) {
-				elevator.setTargetPosition(30);
+			else if(rjoystick.getRisingEdge(3)) {
+				elevator.setTargetPosition(54);
 			}
-			else if(rjoystick.getRawButton(3)) {
-				elevator.setTargetPosition(45);
+			else if(rjoystick.getRisingEdge(4)) {
+				elevator.setTargetPosition(78);
 			}
-			else if(rjoystick.getRawButton(4)) {
-				elevator.setTargetPosition(60);
+			else if(ljoystick.getRisingEdge(2)) {
+				elevator.setTargetPosition(0);
 			}
+			else if(ljoystick.getRisingEdge(4)) {
+				elevator.setTargetPosition(20);
 			
 			/*else {
 				//Percent voltage mode
 				elevator.driverControl(leftYstick);
 			}*/
-			
-		if(ljoystick.getRawButton(1)) {
-			intake.eject();
+		
+			if (ljoystick.getRisingEdge(3)){
+				hatMode = ! hatMode; 
 			}
-			else if(ljoystick.getRawButton(2)) {
-			intake.intake();
+			if (hatMode) {
+				if(ljoystick.getRisingEdge(1)){
+					hat.leftExtend(); 
+				}
+				if(rjoystick.getRisingEdge(1)) {
+					hat.rightExtend();
+				}
+			}
+			else {
+				if(ljoystick.getRisingEdge(1)) {
+					clawUp = !clawUp;
+					if(clawUp){
+						claw.up();
+					}else{
+						claw.down();
+					}
+					
+				}
+				if(rjoystick.getRisingEdge(1)) {
+					intake.eject(); 
+				}
 			}
 
 	}
