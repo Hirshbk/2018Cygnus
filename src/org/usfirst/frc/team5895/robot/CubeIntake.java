c982d971cfb7c3005132152b955e7819fee5b87d
bmacedo-18 <35277180+bmacedo-18@users.noreply.github.com>
2018-01-27 16:20:57 -0500

    edited intake w/o analog sensor
    deleted analog sensors. changed motor speeds for the different modes

--- src/org/usfirst/frc/team5895/robot/CubeIntake.java
+++ src/org/usfirst/frc/team5895/robot/CubeIntake.java
@@ -12,18 +12,19 @@ public class CubeIntake {
 	private Mode_Type mode = Mode_Type.EJECTING;
 	private Talon motor1, motor2;
 	private Solenoid solenoid;
-	private boolean solenoidState;
-	private DistanceSensor sensor1, sensor2;
 	private Timer timer;
-	double motorspeed1, motorspeed2, volts1, volts2, x1, x2, avgDist, speed, lastTime;
-	boolean haveCube;
+	private DigitalInput sensor;
+	double motorspeed1, motorspeed2, lastTime;
+	private boolean solenoidState;
+	boolean lastHasCube;
+	
 	
 	public CubeIntake (){
 		motor1 = new Talon(3);
 		motor2 = new Talon(4);
 		solenoid = new Solenoid (0);
-		sensor1 = new DistanceSensor(); 
-		sensor2 = new DistanceSensor(); 
+		sensor = new DigitalInput(5);
+		lastHasCube=false;
 		}
 
 	public void intake(){
@@ -35,34 +36,12 @@ public class CubeIntake {
 		}
 	
 	public void waiting(){
-		double lastTime = timer.getFPGATimestamp();
 		mode = Mode_Type.WAITING; 
 	}
 	
-	public double setSpeed1() {
-		x1 = sensor1.getDistance();
-	    x2 = sensor2.getDistance();
-	    
-		avgDist = (x1+x2)/2;
-        speed = 16.316*Math.pow(avgDist+20, -0.844367); 
-        
-        motorspeed1 =  speed/4 + x1/45; 
-        
-        return motorspeed1;
-	}
-	
-	public double setSpeed2() {
-		x1 = sensor1.getDistance();
-	    x2 = sensor2.getDistance();
-	    
-		avgDist = (x1+x2)/2;
-        speed = 16.316*Math.pow(avgDist+20, -0.844367); 
-        
-        motorspeed2 = speed/4 + x2/45;
-        
-        return motorspeed2;
-	}
 	public void update(){
+
+		boolean hasCube = !sensor.get();
 		
 		motorspeed1 = 0;
 		motorspeed2 = 0;
@@ -70,57 +49,40 @@ public class CubeIntake {
 		switch(mode) {
 		
 		case INTAKING:
-			 x1 = sensor1.getDistance();
-		     x2 = sensor2.getDistance();
 		     
-		     if(x1 < 5 || x2 < 5) {
-		    	 haveCube = true; //once the cube is close enough to claw sensor, it is stable enough to hold
-		     }
-		     else if ( (x1 < 20 && x1>= 5)|| (x2 < 20 && x2>=5)) { // if 5<distance<20
-		            
-		            setSpeed1();
-		            setSpeed2();
-		     }
-		     else {
-		    	 //if the box is far from our claw, rollers go full speed 
-		            motorspeed1 = 1.0; 
-		            motorspeed2 = 1.0;        
-		      }
-			
-			 if (haveCube){
+		     motorspeed1=.5;
+		     motorspeed2=.5;
+		     
+		     if((lastHasCube == false) && (hasCube)) {
 				lastTime = timer.getFPGATimestamp(); //stamp the time we start waiting 
 			 	mode = Mode_Type.WAITING; //once we have the cube, we prepare to hold and clamp
 			 }
-			
 			solenoidState = false; //solenoid only clamps once it is holding 
 			break;
 			
 		case WAITING:
 		
-			setSpeed1();
-			setSpeed2();
-            
+			motorspeed1=.5;
+			motorspeed2=.5;
+			
         	double curTime = timer.getFPGATimestamp(); //stamps current time 
             if (curTime - lastTime > 0.2) { //compares the time we started waiting to current time
-            	if (haveCube) {
-            		mode = Mode_Type.HOLDING; //if it has been waiting for 200ms, it begins to hold
-            	}else {
-            		mode = Mode_Type.WAITING; //if not, it keeps holding
-            	}
+            	mode = Mode_Type.HOLDING; //if it has been waiting for 200ms, it begins to hold
+            } else {
+            	mode = Mode_Type.WAITING; //if not, it keeps holding
             }
-            
             solenoidState=false; 
 			break;
 		
 		case HOLDING:
-			motorspeed1 = 0;
-			motorspeed2 = 0;
+			motorspeed1 = .2;
+			motorspeed2 = .2;
 			solenoidState = true; // solenoid used to clamp cube while holding 
 			break;
 		
 		case EJECTING:
-			motorspeed1 = -1;
-			motorspeed2 = -1;
+			motorspeed1 = -.5;
+			motorspeed2 = -.5;
 			solenoidState = false; 
 			break; 
 			}
@@ -131,5 +93,3 @@ public class CubeIntake {
 		
 	}
 }
-	
-
