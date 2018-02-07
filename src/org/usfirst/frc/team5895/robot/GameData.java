84204d07dd9025fd1b0954a0895971c878c2ef98
Daniel Cheng <danielcheng0105@gmail.com>
2018-02-07 16:33:04 -0500

    GameData Auto

--- src/org/usfirst/frc/team5895/robot/GameData.java
+++ src/org/usfirst/frc/team5895/robot/GameData.java
@@ -1,16 +1,20 @@
 package org.usfirst.frc.team5895.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class GameData {
 
 	String gameData; 
+	String side;
+	
 	boolean leftSwitch, leftScale; {
 		leftSwitch = true; 
 		leftScale = true;
 	}
 	
-	public void GameData() {
+		
+		side = SmartDashboard.getString("DB/String 0", "nothing");
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
 		if(gameData.charAt(0) == 'L'){
 			leftSwitch= true;
@@ -27,16 +31,79 @@ public class GameData {
 		} else  {
 			DriverStation.reportError("Did not read field data", false);
 		}
-		
 	} 
-
-	public boolean getLeftSwitch() {
-		return leftSwitch;
+	//right side, right switch, right scale
+	public boolean RRR() {
+		if (side.toUpperCase().contains("R")
+				&& leftSwitch == false
+				&& leftScale == false)
+			return true;
+		else
+			return false;
 	}
-	public boolean getLeftScale() {
-		return leftScale;
+	//right side, right switch, left scale
+	public boolean RRL() {
+		if (side.toUpperCase().contains("R")
+				&& leftSwitch == false
+				&& leftScale == true)
+			return true;
+		else
+			return false;
 	}
-	
+	//right side, left switch, right scale
+	public boolean RLR() {
+		if (side.toUpperCase().contains("R")
+				&& leftSwitch == true
+				&& leftScale == false)
+			return true;
+		else
+			return false;
+	}
+	//right side, left switch, left scale
+	public boolean RLL() {
+		if (side.toUpperCase().contains("R")
+				&& leftSwitch == true
+				&& leftScale == true)
+			return true;
+		else
+			return false;
+	}
+	//left side, left switch, left scale
+	public boolean LLL() {
+		if (side.toUpperCase().contains("L")
+				&& leftSwitch == true
+				&& leftScale == true)
+			return true;
+		else
+			return false;
+	}
+	//left side, left switch, right scale
+	public boolean LLR() {
+		if (side.toUpperCase().contains("L")
+				&& leftSwitch == true
+				&& leftScale == false)
+			return true;
+		else
+			return false;
+	}
+	//left side, right switch, left scale
+	public boolean LRL() {
+		if (side.toUpperCase().contains("L")
+				&& leftSwitch == false
+				&& leftScale == true)
+			return true;
+		else
+			return false;
+	}
+	//left side, right switch, right scale
+	public boolean LRR() {
+		if (side.toUpperCase().contains("L")
+				&& leftSwitch == false
+				&& leftScale == false)
+			return true;
+		else
+			return false;
+	}
 }
 
 	
