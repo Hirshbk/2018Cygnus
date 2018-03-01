8ed7baddbe5b8fdcfe4da716db6269c6f8a84822
Arthur <lalewis-19@peddie.org>
2018-03-01 17:02:36 -0500

    updated limelight seek

--- src/org/usfirst/frc/team5895/robot/Limelight.java
+++ src/org/usfirst/frc/team5895/robot/Limelight.java
@@ -1,13 +1,16 @@
 package org.usfirst.frc.team5895.robot;
 
+import org.usfirst.frc.team5895.robot.framework.Waiter;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class Limelight {
-	
+
 	// make sure to use the new library that doesn't have "wpilibj" in the path
 	private NetworkTable table;
-	
+
 	// input
 	private boolean hasTarget;
 	private double horizontalOffset; // -27 to 27 degrees
@@ -15,43 +18,43 @@ public class Limelight {
 	private double area; // % of image
 	private double rotation; // -90 to 0 degrees
 	private double latency; // ms (Add at least 11ms for image capture latency)
-	
+
 	// output
 	private LedMode led = LedMode.ON;
 	private CamMode cam = CamMode.VISION_PROCESSING; // operation mode
 	private double pipeline = 0; // current pipeline
-	
+
 	/**
 	 * Sets enum types of LED mode: ON, OFF, BLINKING
 	 * 
 	 */
 	public enum LedMode {
 		ON(0),OFF(1),BLINKING(2);
-		
+
 		private double value;
-		
+
 		LedMode(double value){
 			this.value = value;
 		}
-		
+
 		public double getValue() {
 			return value;
 		}
 	}
-	
+
 	/**
 	 * Sets enum types of Camera mode: VISION_PROCESSING (Vision mode), DRIVER_CAMERA (Raw Image)
 	 *
 	 */
 	public enum CamMode {
 		VISION_PROCESSING(0),DRIVER_CAMERA(1);
-		
+
 		private double value;
-		
+
 		CamMode(double value){
 			this.value = value;
 		}
-		
+
 		public double getValue() {
 			return value;
 		}
@@ -63,9 +66,9 @@ public class Limelight {
 	 */
 	public Limelight() {
 		NetworkTableInstance.getDefault().startClient(); //
-        table = NetworkTableInstance.getDefault().getTable("limelight");
+		table = NetworkTableInstance.getDefault().getTable("limelight");
 	}
-	
+
 	/**
 	 * Update all methods in need of routine refreshing
 	 * 
@@ -80,27 +83,46 @@ public class Limelight {
 		updateLedMode();
 		updateCamMode();
 	}
-	
+
 	/**
 	 * seeks for a cube assuming that its sees one and does not already possess one.
 	 * @param intake cube intake instance
 	 * @param drive drive train instance.
 	 */
-	public void seek(CubeIntake intake, DriveTrain drive) {
-		while (!intake.hasCube() && hasTarget) {
-			double threshold = 10, speed = .5;
-			// go forward
-			if (Math.abs(horizontalOffset) < threshold) {
-				drive.arcadeDrive(speed, 0);
-			}
+	public boolean seek(CubeIntake intake, DriveTrain drive) {
+		if (!intake.hasCube() && hasTarget) {
+			intake.down();
+			intake.intake();
+			double threshold = 10, speed = 0.2;
 			// turn to face cube
-			else {
-				drive.turnTo(horizontalOffset);
+
+			DriverStation.reportError("horizontal: " + horizontalOffset, false);
+			if (Math.abs(horizontalOffset) > threshold) {
+				DriverStation.reportError("turning", false);
+				if (horizontalOffset > 0)
+					drive.turnTo(horizontalOffset+10);
+				else 
+					drive.turnTo(horizontalOffset-10);
+				//Waiter.waitFor(2000);
 			}
+			// go forward
+			else {
+				DriverStation.reportError("forward", false);
+				drive.arcadeDrive(-speed, 0);
+				//Waiter.waitFor(2000);
+				
+			}
+			return true;
 		}
-		drive.stopTurning();
+
+		return false; //no target || has cube
 	}
 	
+	public void autoSeek(CubeIntake intake, DriveTrain drive) {
+		while (seek(intake, drive)) {}
+		drive.arcadeDrive(0, 0);
+	}
+
 	/**
 	 * Update boolean hasTarget
 	 */
@@ -129,11 +151,11 @@ public class Limelight {
 	public void updateRotation() {
 		rotation = table.getEntry("ts").getDouble(-1);
 	}
-
+	
 	public void updateLatency() {
 		latency = table.getEntry("tl").getDouble(-1);
 	}
-
+	
 	public void updateLedMode() {
 		table.getEntry("ledMode").setDouble(led.getValue());
 	}
@@ -193,5 +215,5 @@ public class Limelight {
 	public double getPipeline() {
 		return pipeline;
 	}
-	
+
 }
